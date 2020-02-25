dbbbe8eae4fcb3364238f809364c9a139991c307
Samarth Nayyar <samarthnayyar123@gmail.com>
2020-02-26 01:25:39 +0530

    Added how we can help section and scrolls

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,9 @@ $(document).ready(function () {
         $('.tint').fadeOut(500);
         setTimeout(() => {
             $('.landing-video').addClass('landing-video-ani');
+            $('html, body').animate({
+                scrollTop: $("#right-video").offset().top
+            }, 500);
         }, 500);  
         $('#expand-video').fadeOut();
         $('#close-video').fadeIn();
@@ -39,4 +42,44 @@ $(document).ready(function () {
             video.setAttribute("controls", "controls")
         }
     }
+
+    $('#phone-option').click(function(){
+        $('#phone-option').addClass('active-option');
+        $('#email-option').removeClass('active-option');
+        $('#letter-option').removeClass('active-option');
+        $('#phone-actions').fadeIn(400);
+        $('#email-actions').fadeOut(0);
+        $('#letter-actions').fadeOut(0);
+    })
+
+    $('#email-option').click(function(){
+        $('#email-option').addClass('active-option');
+        $('#phone-option').removeClass('active-option');
+        $('#letter-option').removeClass('active-option');
+        $('#email-actions').fadeIn(400);
+        $('#letter-actions').fadeOut(0);
+        $('#phone-actions').fadeOut(0);
+    })
+
+    $('#letter-option').click(function(){
+        $('#letter-option').addClass('active-option');
+        $('#email-option').removeClass('active-option');
+        $('#phone-option').removeClass('active-option');
+        $('#letter-actions').fadeIn(400);
+        $('#email-actions').fadeOut(0);
+        $('#phone-actions').fadeOut(0);
+    })
+
+    $("#know-more").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#mission").offset().top
+        }, 1000);
+    });
+
+    $("#action").click(function (){
+        $('html, body').animate({
+            scrollTop: $("#form-options").offset().top
+        }, 1000);
+    });
+
 })
